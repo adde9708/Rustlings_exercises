fd8e83df3a5dd6c2408b09ec838b89995b9702d5
adde9708 <adde.edvin@gmail.com>
2023-06-20 01:20:15 +0200

    finished all the move_semantics exercises

--- rustlings/exercises/move_semantics/move_semantics5.rs
+++ rustlings/exercises/move_semantics/move_semantics5.rs
@@ -3,13 +3,23 @@
 // adding, changing or removing any of them.
 // Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
+    //Assign 100 to x
     let mut x = 100;
+
+    // Create the variable y which points to x
     let y = &mut x;
-    let z = &mut x;
+
+    //Increment y by 100 and remember that y is still x,
+    //so basically this just increments x by 100
     *y += 100;
+
+    /*
+      These two lines does the exact same thing as what
+      was done with the y variable
+    */
+    let z = &mut x;
     *z += 1000;
+
     assert_eq!(x, 1200);
 }
