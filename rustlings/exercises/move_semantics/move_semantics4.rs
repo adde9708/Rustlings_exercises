5d59cca6af8fef7b0de728a0005e064ca27c72ee
adde9708 <adde.edvin@gmail.com>
2023-06-20 00:49:09 +0200

    solved another couple of exercises

--- rustlings/exercises/move_semantics/move_semantics4.rs
+++ rustlings/exercises/move_semantics/move_semantics4.rs
@@ -4,12 +4,9 @@
 // function.
 // Execute `rustlings hint move_semantics4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let vec0 = Vec::new();
-
-    let mut vec1 = fill_vec(vec0);
+    //Create variable to get the return value of the function
+    let mut vec1 = fill_vec();
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -20,11 +17,14 @@ fn main() {
 
 // `fill_vec()` no longer takes `vec: Vec<i32>` as argument
 fn fill_vec() -> Vec<i32> {
-    let mut vec = vec;
+    // create a new empty vector
+    let vec = Vec::new();
+    //assign vec0 to vec
+    let mut vec0 = vec;
+    //put values into vec0
+    vec0.push(22);
+    vec0.push(44);
+    vec0.push(66);
 
-    vec.push(22);
-    vec.push(44);
-    vec.push(66);
-
-    vec
+    vec0
 }
