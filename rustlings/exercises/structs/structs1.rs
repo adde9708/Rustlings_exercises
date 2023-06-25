e942313c71cbad320246de0e259be7c60f44d332
adde9708 <adde.edvin@gmail.com>
2023-06-25 19:42:50 +0200

    solved struct1.rs

--- rustlings/exercises/structs/structs1.rs
+++ rustlings/exercises/structs/structs1.rs
@@ -2,14 +2,22 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint structs1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
+// Create a C-like struct
+// Access it exactly like in C
+// So assign it to a variable then just set the values in the same way
+// that you created it
 struct ColorClassicStruct {
-    // TODO: Something goes here
+    red: i32,
+    green: i32,
+    blue: i32,
 }
 
-struct ColorTupleStruct(/* TODO: Something goes here */);
+// This is a tuple struct so you can access the elements as a tuple
+// By first assigning it to a variable and then accessing it normally
+// let green = ColorTupleStruct and then access it like this green.0
+struct ColorTupleStruct(i32, i32, i32);
 
+// This is a unitlike struct which is a struct that contains no fields
 #[derive(Debug)]
 struct UnitLikeStruct;
 
@@ -19,8 +27,11 @@ mod tests {
 
     #[test]
     fn classic_c_structs() {
-        // TODO: Instantiate a classic c struct!
-        // let green =
+        let green = ColorClassicStruct {
+            red: 0,
+            green: 255,
+            blue: 0,
+        };
 
         assert_eq!(green.red, 0);
         assert_eq!(green.green, 255);
@@ -29,8 +40,7 @@ mod tests {
 
     #[test]
     fn tuple_structs() {
-        // TODO: Instantiate a tuple struct!
-        // let green =
+        let green = ColorTupleStruct(0, 255, 0);
 
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
@@ -39,8 +49,7 @@ mod tests {
 
     #[test]
     fn unit_structs() {
-        // TODO: Instantiate a unit-like struct!
-        // let unit_like_struct =
+        let unit_like_struct = UnitLikeStruct;
         let message = format!("{:?}s are fun!", unit_like_struct);
 
         assert_eq!(message, "UnitLikeStructs are fun!");
