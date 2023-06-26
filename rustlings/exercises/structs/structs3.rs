5a7ee2a1df09cb6822d56ac461e46c89743c2a61
adde9708 <adde.edvin@gmail.com>
2023-06-26 17:50:02 +0200

    solved structs3.rs

--- rustlings/exercises/structs/structs3.rs
+++ rustlings/exercises/structs/structs3.rs
@@ -25,13 +25,17 @@ impl Package {
             }
         }
     }
-
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    // Check if the package will ship internationally
+    fn is_international(&self) -> bool {
+        if self.sender_country != self.recipient_country {
+            true
+        } else {
+            false
+        }
     }
-
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
-        // Something goes here...
+    // Get the price to ship the package based on the weight
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
+        self.weight_in_grams * cents_per_gram
     }
 }
 
